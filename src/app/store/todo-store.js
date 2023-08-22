45b35ea64d2b7c0dcf45eff971ce84eaa62a6b23
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-08-22 14:01:22 -0400

    localStorage

--- src/app/store/todo-store.js
+++ src/app/store/todo-store.js
@@ -1,16 +1,36 @@
 import { create } from "zustand";
 
+const getInitialState = () => {
+  if (typeof window !== "undefined") {
+    const todo = window.localStorage.getItem("todos-list");
+    if (todo) return JSON.parse(todo);
+    return [{ id: 0, title: "Crear una tarea", completed: true }];
+  }
+};
+
 export const useTodoStore = create((set) => ({
-  todos: [{ id: 0, title: "Crear una tarea", completed: true }],
+  todos: getInitialState(),
   addTodo: (todo) => {
     set((state) => {
       const currentTodosAdd = [...state.todos, todo];
+      if (typeof window !== "undefined") {
+      window.localStorage.setItem(
+        "todos-list",
+        JSON.stringify(currentTodosAdd)
+      );
+      }
       return { todos: currentTodosAdd };
     });
   },
   removeTodo: (id) => {
     set((state) => {
       const currentTodosRemove = state.todos.filter((todo) => todo.id !== id);
+      if (typeof window !== "undefined") {
+      window.localStorage.setItem(
+        "todos-list",
+        JSON.stringify(currentTodosRemove)
+      );
+      }
       return { todos: currentTodosRemove };
     });
   },
@@ -19,13 +39,28 @@ export const useTodoStore = create((set) => ({
       const currentTodosToggle = state.todos.map((todo) =>
         todo.id === id ? { ...todo, completed: !todo.completed } : todo
       );
+      if (typeof window !== "undefined") {
+      window.localStorage.setItem(
+        "todos-list",
+        JSON.stringify(currentTodosToggle)
+      );
+      }
       return {
         todos: currentTodosToggle,
       };
     });
   },
   clearCompleted: () =>
-    set((state) => ({ todos: state.todos.filter((todo) => !todo.completed) })),
+    set((state) => {
+      const currentTodosClear = state.todos.filter((todo) => !todo.completed);
+      if (typeof window !== "undefined") {
+      window.localStorage.setItem(
+        "todos-list",
+        JSON.stringify(currentTodosClear)
+      );
+      }
+      return { todos: currentTodosClear };
+    }),
 }));
 
 export const useFormStore = create((set) => ({
